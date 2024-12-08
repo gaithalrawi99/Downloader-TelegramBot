b338adb795eee9add7b50bda895856d85eca440f
gaithalrawi99 <45469381+gaithalrawi99@users.noreply.github.com>
2024-12-09 01:05:43 +0300

    Update Config.py

--- Config/Config.py
+++ Config/Config.py
@@ -1,7 +1,7 @@
 
 class Config:
    SESSION_NAME = "bot"
-   API_ID = ...
-   API_HASH = ""
-   BOT_TOKEN = ""
-   BOT_USERNAME = "" # without @ example : Testbot
+   API_ID = 28678780
+   API_HASH = "0cc41b26d29794267dae5a8c4e78ae4f"
+   BOT_TOKEN = "7544098608:AAGuTOnaAwnN1R-1NW5dacMNTrTvBO5e77Q"
+   BOT_USERNAME = "@SomDownBot" # without @ example : Testbot
